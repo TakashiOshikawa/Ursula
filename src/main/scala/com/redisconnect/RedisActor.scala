13d6d9c07b7bd793cb659dabb211d4b19ad0fb84
Takashi Oshikawa <abr3tks5200@ezweb.ne.jp>
2015-09-25 09:54:53 +0900

    redis singleton

--- src/main/scala/com/redisconnect/RedisActor.scala
+++ src/main/scala/com/redisconnect/RedisActor.scala
@@ -1,15 +1,20 @@
 package com.redisconnect
-
+import com.redis._
 
 /**
  * Created by oshikawatakashi on 2015/08/31.
  */
 
+object RedisClientSingleton {
+  private val red = new RedisClient("127.0.0.1", 6379)
+  def getInstance(): RedisClient = red
+}
+
 
 object RedisConnect {
-  import com.redis._
 
-  val red = new RedisClient("127.0.0.1", 6379)
+  val red = RedisClientSingleton.getInstance
+
   def set(key: String, value: String): Boolean = red.set(key,value)
   def setex(key: String, time: Int, value: String): Boolean = red.setex(key, time, value)
   def get(key: String): String = red.get(key).getOrElse("")
